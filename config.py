f9f3a1dfceec12154ea6e424dff7dd6c381d3f99
Mrunal Mane <103592003+mrunalmane2515@users.noreply.github.com>
2026-07-31 21:25:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,14 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+
+database_url = os.getenv("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = os.getenv("MAIL_SERVER")
