2aacebc254449ea382c7f1717fa32774f5a3a331
Mrunal Mane <103592003+mrunalmane2515@users.noreply.github.com>
2026-07-31 21:35:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
+    database_url = os.getenv("DATABASE_URL")
 
-
-database_url = os.getenv("DATABASE_URL")
-
-if database_url and database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
 
     SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
