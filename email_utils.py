48f6b6607872c9f6fa50b1169d38467adc5b3ab3
Mrunal Mane <103592003+mrunalmane2515@users.noreply.github.com>
2026-07-31 22:19:47 +0530

    Update email_utils.py

--- email_utils.py
+++ email_utils.py
@@ -2,6 +2,7 @@ from flask_mail import Message
 from flask import render_template, current_app
 from extensions import mail
 
+
 def send_task_reminder(user_email, task_title, due_date):
     msg = Message(
         subject="📌 Smart Planner - Task Reminder",
@@ -18,6 +19,27 @@ def send_task_reminder(user_email, task_title, due_date):
 
     try:
         mail.send(msg)
-        print("✅ Email sent successfully")
+        print("✅ Reminder email sent successfully")
     except Exception as e:
-        print(f"❌ Email failed: {e}")
+        print(f"❌ Reminder email failed: {e}")
+
+
+def send_task_assigned_email(user_email, task_title, due_date):
+    msg = Message(
+        subject="📌 Smart Planner - New Task Assigned",
+        sender=current_app.config["MAIL_USERNAME"],
+        recipients=[user_email]
+    )
+
+    msg.html = render_template(
+        "emails/task_assigned.html",
+        task_title=task_title,
+        due_date=due_date,
+        dashboard_url="https://smart-planner-x43e.onrender.com/"
+    )
+
+    try:
+        mail.send(msg)
+        print("✅ Assignment email sent successfully")
+    except Exception as e:
+        print(f"❌ Assignment email failed: {e}")
